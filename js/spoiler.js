dcbf525b26b09d60d713cb682f846e5777880de2
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2022-01-17 04:41:15 +0400

    start

---
+++ js/spoiler.js
@@ -0,0 +1,33 @@
+const spoilers = document.querySelectorAll(".spoiler");
+if (spoilers.length > 0) {
+	spoilers.forEach(spoiler => {
+		const spoilerItem = spoiler.querySelectorAll(".spoiler__item");
+		if (spoilerItem.length > 0) {
+			spoilerItem.forEach(item => {
+				const spoilerText = item.querySelector(".spoiler__text")
+				item.addEventListener("click", () => {
+					const height = spoilerText.scrollHeight;
+					if (item.classList.contains("active")) {
+						item.classList.remove("active");
+						spoilerText.style.maxHeight = "0";
+					} else {
+						spoilerItem.forEach(si => {
+							si.classList.remove("active");
+							si.querySelector(".spoiler__text").style.maxHeight = "0";
+						});
+						item.classList.add("active");
+						spoilerText.style.maxHeight = height + "px";
+					}
+				});
+				document.addEventListener("click", (event) => {
+					if (!event.target.closest(".spoiler")) {
+						spoilerItem.forEach(si => {
+							si.classList.remove("active");
+							si.querySelector(".spoiler__text").style.maxHeight = "0";
+						});
+					}
+				});
+			});
+		}
+	});
+}
